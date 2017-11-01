824e8b4b0d16f2141a1ea4a197074c9194dc7816
Yaroslav Halchenko <debian@onerussian.com>
2017-11-01 09:58:52 -0400

    ENH: preseed RNG for tests, more verbosity in tests output

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -4,10 +4,11 @@ import matplotlib
 
 matplotlib.use('Agg')
 os.environ.update({
-  'MVPA_TESTS_LABILE': 'no',
-  'MVPA_TESTS_LOWMEM': 'yes',
+  'MVPA_TESTS_LABILE': 'no',   
+  'MVPA_TESTS_LOWMEM': 'yes',  # stay friendly
+  'MVPA_SEED': '1',            # not a time to "sweep"
 })
 
 import mvpa2
 
-mvpa2.test(verbosity=2, exit_=True)
+mvpa2.test(verbosity=3, exit_=True)
